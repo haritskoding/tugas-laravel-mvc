55fb8ba822e9719f288c27e86f86a9020975a69d
haritskoding <haritscoding@gmail.com>
2020-07-01 10:08:01 +0700

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function form(){
+        return view('form');
+    }
+
+    public function sapa(Request $request){
+       // var_dump($request);
+       // dd($request["alamat"]);
+       dd($request["alamat"]);
+        return "halo";
+    }
+    public function sapa_post(Request $request){
+        // dd($request->all());
+        $nama = $request["nama"];
+        $alamat = $request["alamat"];
+        return "$nama dan $alamat";
+    }
+}
